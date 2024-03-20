b3c17133718552c7ec39a1fbe2b806de728328cb
sonproject123 <sonproject123@gmail.com>
2024-03-20 19:46:56 +0900

    2024.03.20 반복문

--- Solving/Solve.c
+++ Solving/Solve.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 int main() {
-    int T, C, i;
-    scanf("%d", &T);
+    int x, y, w, h, min = -1;
+    scanf("%d %d %d %d", &x, &y, &w, &h);
 
-    for (i = 0; i < T; i++) {
-        scanf("%d", &C);
-        printf("%d ", C / 25);
-        C %= 25;
-        printf("%d ", C / 10);
-        C %= 10;
-        printf("%d ", C / 5);
-        C %= 5;
-        printf("%d\n", C);
-    }
+    if (w - x > min)
+        min = w - x;
+    if (h - y > min)
+        min = h - y;
+    if ((w - x) * (-1) > min)
+        min = (w - x) * (-1);
+    if ((h - y) * (-1) > min)
+        min = (h - y) * (-1);
 
+    printf("%d\n", min);
     return 0;
 }
